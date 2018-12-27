5ea1855e074ffd5abf36403b504addbaf18dbfce
LanCoder <broderick_wang@163.com>
2018-12-27 17:52:07 +0800

    init

---
+++ wordtopdf/src/main/java/com/eziosc/wordtopdf/WordtopdfApplication.java
@@ -0,0 +1,14 @@
+package com.eziosc.wordtopdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordtopdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordtopdfApplication.class, args);
+    }
+
+}
+
---
+++ wordtopdf/src/main/java/com/eziosc/wordtopdf/controller/InlineImageWordToHtmlConverter.java
@@ -0,0 +1,26 @@
+package com.eziosc.wordtopdf.controller;
+
+import org.apache.poi.hwpf.converter.WordToHtmlConverter;
+import org.apache.poi.hwpf.usermodel.Picture;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.util.Base64;
+
+public class InlineImageWordToHtmlConverter extends WordToHtmlConverter {
+    public InlineImageWordToHtmlConverter(Document document) {
+        super(document);
+    }
+    @Override
+    protected void processImageWithoutPicturesManager(Element currentBlock,
+                                                      boolean inlined, Picture picture)
+    {
+        Element imgNode = currentBlock.getOwnerDocument().createElement("img");
+        StringBuilder sb = new StringBuilder();
+        sb.append(Base64.getMimeEncoder().encodeToString(picture.getRawContent()));
+        sb.insert(0, "data:"+picture.getMimeType()+";base64,");
+        imgNode.setAttribute("src", sb.toString());
+        currentBlock.appendChild(imgNode);
+    }
+
+}
